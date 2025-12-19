2ab326777bc286d30642174cf7f027583de26494
Luka Gavril <luka.gavril@student.tugraz.at>
2025-12-19 16:18:28 +0100

    Added the start to GUI and basic data loading from file

--- src/main/java/com/example/todo/Main.java
+++ src/main/java/com/example/todo/Main.java
@@ -1,13 +1,21 @@
 package com.example.todo;
 
+import javax.swing.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    private static void createDataDirFile() {
+    private static List<Todo> todos = new ArrayList<>();
+
+    private static File createDataDirFile() {
         File dataDirectory = new File("data");
         File file = new File(dataDirectory, "todos.txt");
 
@@ -26,31 +34,74 @@ public class Main {
             } else {
                 System.out.println("File already exists");
             }
+            return file;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    /// should work on all OS
-    private static void createDataWithPath() {
+    private static Todo makeTodoFromFileLine(String line) {
+        // completed;title
+        // todo add checks for invalid lines
+        String[] parts = line.split(";");
+        boolean completed = parts[0].equals("1");
+        return new Todo(completed, parts[1]);
+    }
+
+    private static List<Todo> loadOldTodosFromFile(File file) {
+        try(Scanner scanner = new Scanner(file)) {
+            List<Todo> todos = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                todos.add(makeTodoFromFileLine(line));
+            }
+            return todos;
+
+        }catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // TODO Path???
+    private static File createDataWithPath() {
 
         Path dir = Path.of("data");
-        Path file = dir.resolve("todos.txt");
+        Path filepath = dir.resolve("todos.txt");
 
         try {
             Files.createDirectories(dir);
-            Files.createFile(file);
+            File file = Files.createFile(filepath).toFile();
+            return file;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
     public static void main(String[] args) {
 
-        //createDataDirFile(); // og
-        createDataWithPath(); // improvement
+        File file = createDataDirFile(); // og
+        //File file = createDataWithPath(); // todo doesn't work when file already exists
+        if (file == null) {
+            System.out.println("file is null");
+            return; // todo exit codes in java
+        }
 
+        todos = loadOldTodosFromFile(file);
 
+        // Always start Swing apps on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Todo List");
+
+            frame.setSize(400, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null); // center on screen
+
+            frame.setVisible(true);
+        });
 
     }
 }
---
+++ src/main/java/com/example/todo/Todo.java
@@ -0,0 +1,27 @@
+package com.example.todo;
+
+public class Todo {
+    // todo make them private?
+    final int id;
+    final boolean completed;
+    final String title;
+    // todo add priority, due date
+
+    public Todo (boolean c, String t) {
+        this.id = 2 + t.length();
+        completed = c;
+        title = t;
+    }
+
+    public Todo (int id, boolean c, String t) {
+        this.id = id;
+        completed = c;
+        title = t;
+    }
+
+    @Override
+    public String toString() {
+        String completed = this.completed ? "1" : "0";
+        return completed + ": " + this.title;
+    }
+}
